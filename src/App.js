1ad0374daa0940174bdb5351d5da1cf4521275dc
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-08-25 17:11:47 +0700

    Create Search and Destination section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from "./components/Navbav";
 import Hero from "./components/Hero";
+import Destinations from "./components/Destinations";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <Navbar />
         <Hero />
+        <Destinations />
+      <Search />
     </div>
   );
 }
